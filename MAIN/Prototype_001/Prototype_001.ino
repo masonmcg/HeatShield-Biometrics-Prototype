f560de065e41a4d6ea200070dc4e19332462990b
meep779 <meep779@gmail.com>
2024-04-17 21:23:16 -0700

    yeee

--- MAIN/Prototype_001/Prototype_001.ino
+++ MAIN/Prototype_001/Prototype_001.ino
@@ -1,21 +1,22 @@
 #include "display.h"
+#include "battery.h"
 
 void setup() {
   Serial.begin(9600);
   // put your setup code here, to run once:
   displayInit();
+  batteryInit();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
    displayPrint(1,"TEST L1", ST77XX_CYAN);
    delay(2000);
-   displayPrint(2,"Line2", ST77XX_RED);
-   delay(2000);
-   displayPrint(3,"Balls", ST77XX_WHITE);
-   delay(2000);
-   displayPrint(4,"Heat RTOKE", ST77XX_GREEN);
-   delay(2000);
-   displayPrint(1,"Line 1 rewrite", ST77XX_GREEN);
-   delay(2000);
+   
+  int bat_per = batteryGetPercentage();
+  char buffer[4]; // Buffer for converting int to string
+  itoa(bat_per, buffer, 10); // Convert int to string
+  displayPrint(2, buffer, ST77XX_RED);
+  delay(2000);
+   
 }
